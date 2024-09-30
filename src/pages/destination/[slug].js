9fb346b262b1953769c9c4239c90f2451b11c064
Gamaliel <sirengogamaliel@gmail.com>
2024-09-30 12:15:43 +0300

    Update [slug].js

--- src/pages/destination/[slug].js
+++ src/pages/destination/[slug].js
@@ -1,9 +1,6 @@
 import Footer from '@/components/sections/Footer/Footer'
 import NavigationBar from '@/components/sections/NavigationBar'
 import DestinationData from '@/components/sections/SingleDestination/DestinationData'
-import DestinationHero from '@/components/sections/SingleDestination/DestinationHero'
-import DestinationHighlights from '@/components/sections/SingleDestination/DestinationHighlights'
-import { getDestinationById } from '@/lib/getDestinationById'
 import React from 'react'
 
 export default function destination() {
@@ -12,8 +9,6 @@ export default function destination() {
   return (
     <div>
      <div className='block sm:hidden'>   <NavigationBar /></div>
-        {/* <DestinationHero /> */}
-        {/* <DestinationHighlights /> */}
         <DestinationData  />
         <Footer />
     </div>
